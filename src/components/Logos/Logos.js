fb87c0dcae22a3e2306aa2030fd2b20c0020aa66
kangwritescode <kangwritescode@gmail.com>
2019-12-26 05:53:22 -0800

    media queries

--- src/components/Logos/Logos.js
+++ src/components/Logos/Logos.js
@@ -8,25 +8,36 @@ import ig from '../../assets/instagram.png'
 const Logos = props => {
   const iconLinks = [
     { url: fb, delay: '3.9s', href: 'https://www.facebook.com/david.kang.75' },
-    { url: octocat, delay: '3.95s', href: 'https://github.com/kangwritescode/' },
-    { url: linkedIn, delay: '4s', href: 'https://www.linkedin.com/in/davidhjkang/' },
+    {
+      url: octocat,
+      delay: '3.95s',
+      href: 'https://github.com/kangwritescode/'
+    },
+    {
+      url: linkedIn,
+      delay: '4s',
+      href: 'https://www.linkedin.com/in/davidhjkang/'
+    },
     { url: ig, delay: '4.05s', href: 'https://www.instagram.com/ajiashi/' }
   ]
+
+  const openLink = url => {
+    window.open(url)
+  }
 
   return (
     <div className='container'>
       {iconLinks.map(link => {
         return (
-          <a href={link.href} target="_blank">
-            <img
-              className='link'
-              alt=''
-              src={link.url}
-              style={{
-                animation: `pop-in-right .15s ease-in-out ${link.delay} forwards`
-              }}
-            ></img>
-          </a>
+          <img
+            className='link'
+            alt=''
+            src={link.url}
+            onClick={() => openLink(link.href)}
+            style={{
+              animation: `pop-in-right .15s ease-in-out ${link.delay} forwards`
+            }}
+          ></img>
         )
       })}
     </div>
