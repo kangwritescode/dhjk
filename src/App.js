8eff32225b054ab75d336a21324ed8b6c1960278
kangwritescode <kangwritescode@gmail.com>
2019-12-30 00:44:34 -0800

    clean slate

--- src/App.js
+++ src/App.js
@@ -1,56 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
-import About from './components/About/About'
-import InfinityChrome from './components/InfinityChrome/InfinityChrome'
-import NavIcon from './components/NavIcon/NavIcon'
-import Logos from './components/Logos/Logos'
+import './components/Landing/Landing'
 
 function App () {
-  const [curr, setCurr] = useState(null)
-  const [prev, setPrev] = useState(null)
-
   useEffect(() => {
-    setCurr('blue')
     return () => {}
   }, [])
 
-  const toggle = () => {
-    setPrev(curr)
-    setCurr('red')
-  }
-
-  let style = { position: 'relative', animation: 'move-left .5s ease-in forwards', color: 'red' }
-
-  let redComponent = <About style={style} />
-  let blueComponent = <About style={{ color: 'blue', animation: curr === 'blue' ? null : 'move-left-out .5s ease-in forwards'}} />
-
-  let currShown
-  if (curr === 'blue') {
-    currShown = blueComponent
-  } else {
-    currShown = redComponent
-  }
-  let lastShown
-  if (prev === 'blue') {
-    lastShown = blueComponent
-  }
-
-  return (
-    <div className='App'>
-      <NavIcon />
-      {/* <InfinityChrome /> */}
-      {/* <IntroName /> */}
-      {/* <Logos /> */}
-      {lastShown}
-      {currShown}
-      <button
-        onClick={toggle}
-        style={{ position: 'absolute', bottom: '200px', zIndex: '9999' }}
-      >
-        Next
-      </button>
-    </div>
-  )
+  return <div className='App'></div>
 }
 
 export default App
---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react'
+import './Landing.css' 
+
+const Landing = (props) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+
+export default Landing
