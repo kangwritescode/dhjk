acdc6da4446d039d751e9173571fba770bbbb4a9
kangwritescode <kangwritescode@gmail.com>
2020-01-04 12:00:34 -0800

    before landing changes

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App () {
 
   return (
     <div className='App'>
-      {/* <Landing /> */}
-      <Portfolio />
+      <Landing />
+      {/* <Portfolio /> */}
       <NavBar />
     </div>
   )
