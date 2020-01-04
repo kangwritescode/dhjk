89a898938a896a985aa70c7b44fd57822b12ecca
kangwritescode <kangwritescode@gmail.com>
2020-01-04 12:35:41 -0800

    new landing look

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -7,6 +7,9 @@ import NavBar from '../NavBar/Navbar/NavBar'
 import RightArrow from '../RightArrow/RightArrow'
 import { AwesomeButton } from 'react-awesome-button'
 
+
+// https://drive.google.com/open?id=1MZdkFUsgY4K_yXKXVn8DwAStC1bo6PAl   <- resume
+
 const Landing = props => {
   return (
     <React.Fragment>
@@ -16,8 +19,14 @@ const Landing = props => {
           <Thoughts />
         </div>
         <img className='landing__head' src={head} alt='hi' />
-        <h1 id='landing-header'>David Kang</h1>
-        <h6 id='landing-subtext'>Web Developer | Problem Solver | Artist</h6>
+        <h1 id='landing-header'>Hi there, I'm David Kang.</h1>
+        <h6 id='landing-subtext'>I'm a recent UC Berkeley graduate and Web Developer. <br/>
+        Articles, labs, and more can be found in the code laboratory. <br/>
+        You can find internet things I've made on the portfolio page. <br/>
+        Best reached any time at davidhjkang@berkeley.edu.
+
+        
+        </h6>
       </div>
       <Logos />
     </React.Fragment>
